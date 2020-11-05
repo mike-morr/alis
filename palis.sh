c8d10b13c445e42d3fc9d2bf3917af7f161ef707
Mike Morrison <mike-morr@users.noreply.github.com>
2020-11-05 04:46:03 +0000

    u

--- palis.sh
+++ palis.sh
@@ -3,7 +3,7 @@
 # mostly an automatic version of https://wiki.archlinux.org/index.php/User:Altercation/Bullet_Proof_Arch_Install
 
 # let's get minimum deps for PowerShell
-pacman --noconfirm --needed -S git fakeroot dotnet-runtime
+pacman --noconfirm --needed -S git fakeroot dotnet-runtime dotnet-sdk
 
 export PATH="${PATH}:~/.dotnet/tools"
 

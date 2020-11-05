0c4682abee5139c0930da61741ad5692c8f43e5a
Mike Morrison <mike-morr@users.noreply.github.com>
2020-11-05 04:16:44 +0000

    u

--- palis.sh
+++ palis.sh
@@ -2,7 +2,9 @@
 pacman --needed -S git fakeroot
 
 # create aur build directory
+rm -rf /home/build || true
 mkdir /home/build
+
 chgrp nobody /home/build
 chmod g+ws /home/build
 setfacl -m u::rwx,g::rwx /home/build
@@ -10,6 +12,7 @@ setfacl -d --set u::rwx,g::rwx,o::- /home/build
 cd /home/build
 
 # Clone and build PowerShell
+rm -rf /home/build/powershell-bin || true
 git clone https://aur.archlinux.org/powershell-bin.git
 cd powershell-bin
 cat PKGBUILD

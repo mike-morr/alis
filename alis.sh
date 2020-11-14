cf06d4eac5ff3feb91c062290b9edb9f3f178af6
Mike Morrison <mike-morr@users.noreply.github.com>
2020-11-14 05:46:27 +0000

    u

--- alis.sh
+++ alis.sh
@@ -720,7 +720,7 @@ EOT
         echo "vm.swappiness=10" > /mnt/etc/sysctl.d/99-sysctl.conf
     fi
 
-    printf "$ROOT_PASSWORD\n$ROOT_PASSWORD" | arch-chroot /mnt passwd
+    printf "$ROOT_PASSWORD\n$ROOT_PASSWORD\n" | arch-chroot /mnt passwd
 }
 
 function mkinitcpio_configuration() {
